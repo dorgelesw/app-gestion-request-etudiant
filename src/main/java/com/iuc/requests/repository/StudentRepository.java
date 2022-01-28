75dd33bcffb524d5b1f401cddca66f92534a73c9
Cahoolessur <poovesh.cahoolessur@accenture.com>
2022-01-28 12:10:28 +0400

    [IUC-REQUEST-2]add student dto, define find by email in student repository, unit test student modelmapper

--- src/main/java/com/iuc/requests/repository/StudentRepository.java
+++ src/main/java/com/iuc/requests/repository/StudentRepository.java
@@ -1,8 +1,12 @@
 package com.iuc.requests.repository;
 
+import com.iuc.requests.dao.Staff;
 import com.iuc.requests.dao.Student;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StudentRepository extends CrudRepository<Student, Long> {
+    Student findByEmail(String email);
+
+    Student findByMatricule(String matricule);
 
 }
--- src/test/java/com/iuc/requests/modelmapper/UserModelMapperTest.java
+++ src/test/java/com/iuc/requests/modelmapper/UserModelMapperTest.java
@@ -4,7 +4,10 @@ import com.iuc.requests.conf.AppConfigurationTest;
 import com.iuc.requests.conf.H2JpaConfiguration;
 import com.iuc.requests.dao.Staff;
 import com.iuc.requests.dto.StaffDto;
+import com.iuc.requests.dao.Student;
+import com.iuc.requests.dto.StudentDto;
 import com.iuc.requests.repository.StaffRepository;
+import com.iuc.requests.repository.StudentRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.After;
 import org.junit.Assert;
@@ -30,15 +33,18 @@ public class UserModelMapperTest {
 
     @Autowired
     StaffRepository staffRepository;
+    StudentRepository studentRepository;
 
     @Before
     public void populate() {
         populator.populateStaff();
+
     }
 
     @After
     public void reset() {
         populator.resetStaff();
+
     }
 
     @Test
@@ -86,5 +92,51 @@ public class UserModelMapperTest {
         assertThat(persistedStaff.getEmail()).isEqualTo(retrievedStaff.getEmail());
         assertThat(persistedStaff.getFiliere()).isEqualTo(retrievedStaff.getFiliere());
         assertThat(persistedStaff.getPassword()).isEqualTo(retrievedStaff.getPassword());
+    }
+    @Test
+    public void testModelMapperStudentToStudentDto() {
+
+        Student student = studentRepository.findByEmail("student-test-1@iuc.com");
+        StudentDto studentDto = modelMapper.map(student, StudentDto.class);
+
+        assertThat(student.getNom()).isEqualTo(studentDto.getNom());
+        assertThat(student.getPrenom()).isEqualTo(studentDto.getPrenom());
+        assertThat(student.getNiveau()).isEqualTo(studentDto.getNiveau());
+        assertThat(student.getMatricule()).isEqualTo(studentDto.getMatricule());
+        assertThat(student.getEmail()).isEqualTo(studentDto.getEmail());
+        assertThat(student.getFiliere()).isEqualTo(studentDto.getFiliere());
+    }
+
+    @Test
+    public void testModelMapperStudentDtoToStudent() {
+
+        StudentDto studentDto = new StudentDto();
+        studentDto.setNom("student-nom-test-3");
+        studentDto.setPrenom("student-prenom-test-3");
+        studentDto.setNiveau("student-level-1-test-3");
+        studentDto.setMatricule("2CIUC2020");
+        studentDto.setEmail("student-test-3@iuc.com");
+        studentDto.setFiliere("MATHEMATIQUE");
+
+        Student student = modelMapper.map( studentDto, Student.class);
+
+        assertThat(student.getNom()).isEqualTo(studentDto.getNom());
+        assertThat(student.getPrenom()).isEqualTo(studentDto.getPrenom());
+        assertThat(student.getNiveau()).isEqualTo(studentDto.getNiveau());
+        assertThat(student.getMatricule()).isEqualTo(studentDto.getMatricule());
+        assertThat(student.getEmail()).isEqualTo(studentDto.getEmail());
+        assertThat(student.getFiliere()).isEqualTo(studentDto.getFiliere());
+
+        student.setPassword("1234");
+        Student persistedStudent = studentRepository.save(student);
+        Student retrievedStudent = studentRepository.findByEmail("student-test-3@iuc.com");
+
+        assertThat(persistedStudent.getNom()).isEqualTo(retrievedStudent.getNom());
+        assertThat(persistedStudent.getPrenom()).isEqualTo(retrievedStudent.getPrenom());
+        assertThat(persistedStudent.getNiveau()).isEqualTo(retrievedStudent.getNiveau());
+        assertThat(persistedStudent.getMatricule()).isEqualTo(retrievedStudent.getMatricule());
+        assertThat(persistedStudent.getEmail()).isEqualTo(retrievedStudent.getEmail());
+        assertThat(persistedStudent.getFiliere()).isEqualTo(retrievedStudent.getFiliere());
+        assertThat(persistedStudent.getPassword()).isEqualTo(retrievedStudent.getPassword());
     }
 }
